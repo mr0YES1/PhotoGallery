63dbe6b10be19f75c152292baa2d6f3b03c36d0c
mr_yes_dev <mryes.develop@gmail.com>
2025-11-07 22:54:01 +0300

    Enabled tailwind styles

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,7 +2,7 @@
 
 export default function Header() {
   return (
-    <header>
+    <header className="bg-white border-b border-gray-200">
       <h1>Welcome to My Application</h1>
     </header>
   );
